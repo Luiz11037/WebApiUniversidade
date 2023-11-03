196dc4219dfdb52761c6f984658bb66a6d45c293
Luiz11037 <luizfelipe31felipe@gmail.com>
2023-11-03 07:56:01 -0300

    Testado

--- Controllers/DisciplinaController.cs
+++ Controllers/DisciplinaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
+using apiUniversidade.Context;
 
 namespace apiUniversidade.Controllers
 {
@@ -11,36 +12,68 @@ namespace apiUniversidade.Controllers
     [Route("[controller]")]
     public class DisciplinaController : Controller
     {
-        private readonly ILogger<DisciplinaController> _logger;
+       private readonly ILogger<CursoController> _logger;
+       private readonly apiUniversidadeContext _context;
 
-        public DisciplinaController(ILogger<DisciplinaController> logger)
+       public DisciplinaController(ILogger<CursoController> logger , apiUniversidadeContext context)
+       {
+        _logger = logger;
+        _context = context;
+       }
+
+       [HttpGet]
+       public ActionResult<IEnumerable<Disciplina>> Get()
+       {
+        var disciplina = _context.Disciplinas.ToList();
+        if(disciplina is null)
+            return NotFound("Achamo não, parcero");
+
+        return disciplina;
+       }
+        
+        [HttpGet("{id:int}", Name="GetDisciplina")]
+        public ActionResult<Disciplina> Get(int id)
         {
-            _logger = logger;
+            var disciplina = _context.Disciplinas.FirstOrDefault(p => p.Id == id);
+            if(disciplina is null)
+                return NotFound("Tem não, parceiro");
+            return disciplina;
         }
-        [HttpGet(Name = "disciplinas")]
-        public List<Disciplina> GetDisciplina(){
-            List<Disciplina> Disciplinas = new List<Disciplina>();
 
-            Disciplina d1 = new Disciplina();
-            d1.Nome = "programação para internet";
-            d1.CargaHoraria = 60;
-            d1.Semestre = 8;
-
-            Disciplina d2 = new Disciplina();
-            d2.Nome = "projeto de Software";
-            d2.CargaHoraria = 60;
-            d2.Semestre = 8;
-
-            Disciplina d3 = new Disciplina();
-            d3.Nome = "Autoria Web";
-            d3.CargaHoraria = 60;
-            d3.Semestre = 8;
-
-            Disciplinas.Add(d1);
-            Disciplinas.Add(d2);
-            Disciplinas.Add(d3);
-
-            return Disciplinas;
+        [HttpPost]
+        public ActionResult Post(Disciplina disciplina){
+            _context.Disciplinas.Add(disciplina);
+            _context.SaveChanges();
+            
+            return new CreatedAtRouteResult("GetDisciplina", 
+            new{ id = disciplina.Id},
+            disciplina);
         }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Disciplina disciplina) 
+        {
+            if(id != disciplina.Id)
+            return BadRequest();
+
+            _context.Entry(disciplina).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(disciplina);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var disciplina = _context.Disciplinas.FirstOrDefault(p => p.Id == id);
+
+            if(disciplina is null)
+                return NotFound();
+
+            _context.Disciplinas.Remove(disciplina);
+            _context.SaveChanges();
+
+            return Ok(disciplina);           
+        }
+
     }
 }
